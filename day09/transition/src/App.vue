2f0347c2172c27450c6c7a5017dafed26a2ca89f
kaito <kaitooshiro1@icloud.com>
2020-11-21 05:43:57 +0900

    DAY09

---
+++ day09/transition/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s;
+  transition: padding-top 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+  padding-top: 20px;
+}
+</style>
