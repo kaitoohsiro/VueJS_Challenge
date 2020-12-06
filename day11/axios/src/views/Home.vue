262b24a30cb19e5ad92c51a641bf62bf295c5fcf
kaito <kaitooshiro1@icloud.com>
2020-12-06 18:53:54 +0900

    DAY11

---
+++ day11/axios/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  },
+  created() {
+    this.$axios.get("apiのurl").then(response => {
+      console.log(response.data);
+    });
+  }
+};
+</script>
